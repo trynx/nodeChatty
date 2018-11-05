91cd8ff2fddf57ffc90017a746d29ce9637a868d
Nico <trynx93@gmail.com>
2018-11-05 16:50:02 +0200

    Add - User connect/disconnect message

--- server/server.js
+++ server/server.js
@@ -1,15 +1,40 @@
 // Library imports
 let express = require('express');
+let socketIO = require('socket.io');
+let http = require('http');
 
 // Local imports
 
 
 // Express config
-var app = express();
 let port = 3000;
+var app = express();
+
+// Socket.io config
+var server = http.createServer(app);
+var io = socketIO(server);
+
+// Event listener
+// User connected
+io.on('connection', (socket) => {
+    console.log('New user connected'); 
+    // User disconnected
+    socket.on('disconnect', (reason) => {
+      console.log('An user disconnected');
+    });
+
+    socket.on('join', (userNickname) => {
+        console.log(`User ${userNickname} connected`);
+
+        socket.broadcast.emit('userConnected', `${userNickname} has connected`);
+    });
+});
+ 
 
 
 // Start express -> start server
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Started on port', port);
 });
+
+
